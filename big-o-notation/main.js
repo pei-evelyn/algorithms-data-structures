25ac57c97346868794f610ef8fbb69b4edafbf3a
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-12-15 13:37:23 -0800

    Explored different types of big o notation

--- big-o-notation/main.js
+++ big-o-notation/main.js
@@ -1,3 +1,6 @@
+// Add Up To examples
+
+// o(n)
 const addUpToOne = n => {
   let total = 0;
   for (let i = 0; i <= n; i++) {
@@ -6,11 +9,11 @@ const addUpToOne = n => {
   return total;
 }
 
+// o(1)
 
 const addUpToTwo = n => {
   return n * (n + 1) / 2;
 }
-
 
 console.log(addUpToOne(6));
 console.log(addUpToTwo(6));
@@ -24,3 +27,28 @@ const t3 = performance.now();
 addUpToTwo(1000000000);
 const t4 = performance.now();
 console.log(`Time elapsed: ${(t4 - t3) / 1000} seconds`)
+
+// Count up and down
+// o(n)
+
+const countUpAndDown = n => {
+  console.log('Going up!');
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  console.log('At the top! \nGoing down...');
+  for (let j = n -1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log('Back down, bye!');
+}
+
+// o(n^2)
+
+const printAllPairs = n => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+}
